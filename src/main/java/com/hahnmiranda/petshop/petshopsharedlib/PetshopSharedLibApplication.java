08238401b0646265067d28fc1bd47cdde18272b2
hahnmiranda <hahnmiranda@gmail.com>
2025-08-09 15:20:35 -0300

    docs: adicionando esqueleto do projeto com arquivos base.

---
+++ src/main/java/com/hahnmiranda/petshop/petshopsharedlib/PetshopSharedLibApplication.java
@@ -0,0 +1,13 @@
+package com.hahnmiranda.petshop.petshopsharedlib;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetshopSharedLibApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PetshopSharedLibApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hahnmiranda/petshop/petshopsharedlib/PetshopSharedLibApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hahnmiranda.petshop.petshopsharedlib;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetshopSharedLibApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
